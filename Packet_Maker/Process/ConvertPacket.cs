8f7abd6ac6125cc7fcb93095a1fa0800d39fecf2
heon_97 <ash5270@gmail.com>
2023-03-28 23:44:34 +0900

    함수 구현 까지 끝

--- Packet_Maker/Process/ConvertPacket.cs
+++ Packet_Maker/Process/ConvertPacket.cs
@@ -5,25 +5,30 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using System.Text.RegularExpressions;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography.X509Certificates;
+using System.ComponentModel.Design;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Packet_Maker.Process
 {
     public class ConvertPacket
     {
         public ConvertPacket() { }
-        private bool ConvertClass(string? str,StreamWriter writer)
+        private bool ConvertClass(string? str, Dictionary<string, StreamWriter> writers)
         {
             if (str == null)
                 return false;
             if (!Regex.IsMatch(str, "class") || str == "")
                 return false;
 
-            string pattern = @"(class @\w+ : @\w+)";
+            string pattern = @"(class.*@\w+.*:.*@\w+)";
             var result = Regex.Match(str, pattern).Value;
 
             //클래스 패턴 매칭
             if (Regex.Match(str, pattern).Value == str && str != "")
             {
+                //loop
                 pattern = @"(?<=@)\w+";
                 var className = Regex.Matches(str, pattern);
 
@@ -36,34 +41,154 @@ namespace Packet_Maker.Process
             else
                 return false;
         }
-        
-        //변수 패턴 매칭
-        private bool ConvertValue(string? str,StreamWriter writer)
-        {
-            if (str == null)
-                return false;
 
-            string pattern = @"(#\w+ @\w+)";
+        //변수 패턴 매칭
+        private bool ConvertValue(string? str, Dictionary<string, StreamWriter> writers)
+        {
+            if (str == null) return false;
+
+            string pattern = @"^(?!.*(#\w+).*(\$\w+)\(.*\))(#\w+ @\w+)$";
             if (!Regex.IsMatch(str, pattern))
                 return false;
-                
+
+
             pattern = @"(?<=#)\w+";
             var valueType = Regex.Match(str, pattern).Value;
 
-
-
+            //요만큼 loop
+            valueType = OptionConfigManager.LanguageConfig.GetProperty(language).GetProperty(valueType).GetString();
             pattern = @"(?<=@)\w+";
-            var valueName =Regex.Match(str, pattern).Value;
-            string re = string.Format("{0} {1};", "uin16_t", valueName);
+            var valueName = Regex.Match(str, pattern).Value;
+            string re = string.Format("{0} {1};", valueType, valueName);
             writer.WriteLine(re);
+
+
+
             Console.WriteLine(re);
             return false;
         }
 
-        private bool ConvertFunc(string? str, StreamWriter writer)
+        private bool ConvertFunc(string? str, Dictionary<string, StreamWriter> writers)
         {
-           
-            return false;
+            if (str == null) return false;
+            //함수인지 체크
+            string pattern = @"(#\w+).*(\$\w+)\(.*\)";
+            if (!Regex.IsMatch(str, pattern))
+                return false;
+
+            bool isOverride = false;
+
+            //override인지 체크
+            pattern = @"(#\w+) (\$\w+)\(.*\)";
+            if (Regex.IsMatch(str, pattern))
+                isOverride = true;
+
+            pattern = @"(?<=#)\w+";
+            var funcReturnType = Regex.Match(str, pattern).Value;
+            funcReturnType = OptionConfigManager.LanguageConfig.GetProperty(language).GetProperty(funcReturnType).GetString();
+            pattern = @"(?<=\$)\w+";
+            var funcName = Regex.Match(str, pattern).Value;
+
+            //함수 인자 확인 
+            //일단 ref 받는지 확인
+            pattern = @"\(([^)]*)\)";
+            var args= Regex.Match(str, pattern).Value;
+            args= args.Remove(0,1);
+            args=args.Remove(args.Length - 1,1);
+            //ref,type,name
+            List<Tuple<bool,string,string>> argumentTypes = new List<Tuple<bool, string, string>>();
+
+            var strs= args.Split(',');
+            for(int i=0; i<strs.Length; i++)
+            {
+                bool isRef = false;
+                string type = "";
+                string name = "";
+                if (strs[i].IndexOf("ref") != -1)
+                    isRef = true;
+                if (strs[i].IndexOf("#")!=-1)
+                {
+                    int start = strs[i].IndexOf("#");
+                    int end = strs[i].IndexOf(" ",start);
+                    if (end == -1)
+                        end = strs[i].Length;
+                    type = strs[i].Substring(start+1, end - start-1);
+                    type = OptionConfigManager.LanguageConfig.GetProperty(language).GetProperty(type).GetString();
+                }
+
+                if(strs[i].IndexOf("@") != -1)
+                {
+                    int start =strs[i].IndexOf("@");
+                    name = strs[i].Substring(start+1, strs[i].Length-start-1);
+                }
+
+                argumentTypes.Add(new Tuple<bool, string, string>(isRef, type, name));
+               
+            }
+
+            string result = "";
+
+
+            //loog 돌려서 두번
+            if(language=="cpp")
+            {
+                result += "public ";
+                result += isOverride?"override ":"";
+                result +=funcReturnType + " ";
+              
+                result += funcName + "(";
+
+                for(int i=0; i< argumentTypes.Count; i++)
+                {
+                    string refCheck = argumentTypes[i].Item1 ? "ref " : "";
+                    result += refCheck + argumentTypes[i].Item2 + " " + argumentTypes[i].Item3+",";
+                }
+                result=result.Remove(result.Length-1,1);
+                result += ")\n";  
+            }
+
+            writer.Write(result);
+
+
+            return true;
+        }
+
+        public bool ConvertFuncIn(string str, Dictionary<string, StreamWriter> writers)
+        {
+            if (str == "" || str == "{" || str=="}"||str=="\n")
+                return false;
+            string pattern = @"#\w+ @\w+";
+            string result = "";
+            if (!Regex.IsMatch(str, pattern))
+            {
+                if (Regex.IsMatch(str, @"return @\w+"))
+                {
+                    result += "\treturn ";
+                    var value = Regex.Match(str, @"(?<=@)\w+").Value;
+                    result += value +";";
+                    writer.WriteLine(result);
+                    return false;
+                }
+                if (Regex.IsMatch(str, @"return"))
+                {
+                    return false;
+                }
+                else
+                    result =str+";";
+            }
+            else
+            {
+                pattern = @"(?<=#)\w+";
+                var valueType = Regex.Match(str, pattern).Value;
+                valueType = OptionConfigManager.LanguageConfig.GetProperty(language).GetProperty(valueType).GetString();
+                pattern = @"(?<=@)\w+";
+                var valueName = Regex.Match(str, pattern).Value;
+
+                result = string.Format("\t{0} {1};",valueType,valueName);
+            }
+
+            writer.WriteLine(result);
+            return true;
         }
 
         public async void Start()
@@ -74,22 +199,38 @@ namespace Packet_Maker.Process
             StreamReader reader = new StreamReader(file);
 
             var cppfile = File.Open("packet.cpp", System.IO.FileMode.OpenOrCreate);
-            StreamWriter writer = new StreamWriter(cppfile);
+            StreamWriter cppWriter = new StreamWriter(cppfile);
+
+            var csfile = File.Open("packet.cs", System.IO.FileMode.OpenOrCreate);
+            StreamWriter csWriter = new StreamWriter(csfile);
+
+
+            Dictionary<string, StreamWriter> writers=new Dictionary<string, StreamWriter>();
+
+            writers.Add("cpp", cppWriter);
+            writers.Add("cs", csWriter);
 
             string? str = "";
-            while (str != "!end")
+            while (str != "~end")
             {
                 str = await reader.ReadLineAsync();
-                if (ConvertClass(str, writer))
+                if (ConvertClass(str, csWriter))
                 {
 
                 }
-                else if (ConvertValue(str, writer))
+                else if (ConvertValue(str, csWriter))
                 {
 
-                }else if(ConvertFunc(str, writer))
+                }
+                else if (ConvertFunc(str, csWriter))
                 {
-
+                    str = await reader.ReadLineAsync();
+                    writer.WriteLine("{");
+                    while (ConvertFuncIn(str, csWriter))
+                    {
+                        str = await reader.ReadLineAsync();
+                    }
+                    writer.WriteLine("}");
                 }
             }
 
--- Packet_Maker/System/OptionConfigManager.cs
+++ Packet_Maker/System/OptionConfigManager.cs
@@ -13,10 +13,8 @@ namespace Packet_Maker
 {
     public static class OptionConfigManager
     {
-        public static Dictionary<string, LanguageConfig> LanguageConfigs
-            = new Dictionary<string, LanguageConfig>();
-
         public static ConfigData? data;
+        public static JsonElement LanguageConfig;
         public static void Init()
         {
             try
@@ -32,37 +30,9 @@ namespace Packet_Maker
                 if (data == null) return;
 
                 var language = File.Open("language.json", FileMode.Open);
-                var jsonLanguage = JsonDocument.Parse(language).RootElement;
+                LanguageConfig = JsonDocument.Parse(language).RootElement;
 
-                for (int i = 0; i < data.convert_language.Count(); i++)
-                {
-                    var curLan = jsonLanguage.GetProperty(data.convert_language[i]);
-                    LanguageConfig curConfig = new LanguageConfig();
-#pragma warning disable CS8601
-                    //8bit - 1byte data
-                    curConfig.S8BitBYTE = curLan.GetProperty("byte").GetString();
-                    curConfig.U8BitBYTE = curLan.GetProperty("ubyte").GetString();
-                    //16bit -2byte data
-                    curConfig.U16BitINT = curLan.GetProperty("ushort").GetString();
-                    curConfig.S16BitINT = curLan.GetProperty("short").GetString();
-                    //32bit -4byte data
-                    curConfig.S32BitINT = curLan.GetProperty("int").GetString();
-                    curConfig.U32BitINT = curLan.GetProperty("uint").GetString();
-                    //64bit-8byte data
-                    curConfig.S64BitINT = curLan.GetProperty("long").GetString();
-                    curConfig.U64BitINT = curLan.GetProperty("ulong").GetString();
-                    //string
-                    curConfig.STRING = curLan.GetProperty("string").GetString();
-                    curConfig.WSTRING = curLan.GetProperty("wstring").GetString();
-                    //32bit -4byte float
-                    curConfig.S32BitFLOAT = curLan.GetProperty("float").GetString();
-                    //64bit -8byte DOBULE
-                    curConfig.S64BitDOBULE = curLan.GetProperty("double").GetString();
-#pragma warning restore CS8601
-
-                    LanguageConfigs.Add(data.convert_language[i], curConfig);
-                }
-
+             
             }catch (Exception e)
             {
                 //Debug.Assert(true,e.ToString());
